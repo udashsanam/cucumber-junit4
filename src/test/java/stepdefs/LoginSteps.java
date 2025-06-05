58d9e6a0cb90e6cdf0455f4def146e4cb28f2ad4
udashsanam <udashsanam@gmail.com>
2025-06-05 14:27:09 +0545

    add: spring dependey as mock

--- src/test/java/stepdefs/LoginSteps.java
+++ src/test/java/stepdefs/LoginSteps.java
@@ -1,16 +1,37 @@
 package stepdefs;
 
+import com.learn.exchangeservice.entity.UserEntity;
+import com.learn.exchangeservice.repo.UserRepo;
+import com.learn.exchangeservice.test.Test;
+import com.learn.test.Test.Tets;
 import io.cucumber.java.en.*;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
+
 
 public class LoginSteps {
 
     private String page = "";
     private boolean authenticated = false;
 
+
+    private UserRepo userRepo = Mockito.mock(UserRepo.class);
+
     @Given("the user is on the login page")
     public void user_is_on_login_page() {
         page = "login";
+        Tets tets = new Tets();
+        tets.tets();
+        Test test = new Test();
+        Mockito.when(userRepo.findAll()).thenReturn(List.of(new UserEntity()));
+        test.print();
+        List<UserEntity> userEntities = userRepo.findAll();
+        System.out.println(userEntities.get(0).getId());
+
     }
 
     @When("the user enters valid credentials")
